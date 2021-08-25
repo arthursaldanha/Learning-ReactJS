ea245b288827b52c8136466e15a15fda47459012
Arthur Saldanha <saldanhadev@gmail.com>
2021-08-25 12:20:26 -0300

    Components e Props - React.JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Avatar(props) {
+   return (
+      <div> 
+         <img src={props.user.url} alt={props.user.name} />
+         <br/>
+         <span>{props.user.name}</span>
+      </div>
+   );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   let user = {
+      url: "https://www.google.com.br/google.jpg",
+      name: "Arthur Saldanha",
+   };
+
+   return <>
+      <Avatar user={user}/>
+   </>
 }
 
 export default App;
