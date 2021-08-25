bb523560e64c44e089a0c224366efde2d99138ff
Arthur Saldanha <saldanhadev@gmail.com>
2021-08-25 17:35:53 -0300

    Aprendendo State Hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 
-const Site = styled.div`
-   width: 500px;
-   height: 500px;
-   background-color: #ccc;
-`;
-
-const Button = styled.button`
-   padding: 10px 15px;
-   font-size: 18px;
-   background-color: #fff;
-   border: 3px solid red;
-   margin: 5px;
-   border-radius: 7px;
-`;
-
-const BotaoPequeno = styled(Button)`
-   padding: 5px 10px;
-   font-size: 14px;
-`;
-
 function App() {
+   const [ contador, setContador ] = useState(89);
+
    return (
-      <Site>
-         <Button>Clique Aquiiii!</Button>
-         <BotaoPequeno>Clique Aquiiii!</BotaoPequeno>
-      </Site>
+      <>
+         <div>{contador} vezes</div>
+         <button onClick={() => setContador( contador + 1 )}>Clique para aumentar</button>
+      </>
    );
 }
 
