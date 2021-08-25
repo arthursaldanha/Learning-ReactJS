588c2b5540f0f9ec7559b4e586fa016d0b08cb55
Arthur Saldanha <saldanhadev@gmail.com>
2021-08-25 17:01:40 -0300

    Reaproveitando estilos com Styled-Components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Site = styled.div`
-   width: 300px;
-   height: 300px;
-   background-color: #00ff00;
-
+   width: 500px;
+   height: 500px;
+   background-color: #ccc;
 `;
 
 const Button = styled.button`
-   text-align: center;
    padding: 10px 15px;
-   font-size: 24px;
-   font-weight: 600;
+   font-size: 18px;
+   background-color: #fff;
+   border: 3px solid red;
+   margin: 5px;
+   border-radius: 7px;
+`;
+
+const BotaoPequeno = styled(Button)`
+   padding: 5px 10px;
+   font-size: 14px;
 `;
 
 function App() {
    return (
       <Site>
          <Button>Clique Aquiiii!</Button>
+         <BotaoPequeno>Clique Aquiiii!</BotaoPequeno>
       </Site>
    );
 }
