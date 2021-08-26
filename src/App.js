e0290b67fbf83659915547ef2a12837685b40c74
Arthur Saldanha <saldanhadev@gmail.com>
2021-08-26 14:31:03 -0300

    Desafio Calculadora de Gorjeta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useState } from 'react';
 import styled from 'styled-components'
 
+const Title = styled.h1``;
+
+const Input = styled.input`
+
+`;
+
 function App() {
-   const [ contador, setContador ] = useState(89);
+   const [ pct, setPct ] = useState(10);
+   const [ conta, setConta ] = useState(0);
 
    return (
       <>
-         <div>{contador} vezes</div>
-         <button onClick={() => setContador( contador + 1 )}>Clique para aumentar</button>
+         <Title>Calculadora de Gorjeta</Title>
+
+         <p>Quanto deu a conta?</p>
+         <Input type="number" value={conta} onChange={(e) => { setConta(e.target.value) }}></Input>
+
+         <p>Qual a porcentagem de gorjeta?</p>
+         <Input type="number" value={pct} onChange={(e) => { setPct(e.target.value) }}></Input>
+
+         <hr />
+
+         { conta > 0 &&
+            <>
+               <p>Sub-total: R$ {conta}</p>
+               <p>Gorjeta ({pct}%): R$ {(parseFloat((pct / 100) * conta)).toFixed(1)}</p>
+               <p>Total: R$ {(parseFloat((pct / 100) * conta) + parseInt(conta)).toFixed(1)}</p>
+            </>
+         }
       </>
    );
+
 }
 
 export default App;
