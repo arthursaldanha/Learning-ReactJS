983518576551f0f83a6404c9a87501b22d1624e6
Arthur Saldanha <saldanhadev@gmail.com>
2021-08-27 17:19:53 -0300

    Conhecendo o JSX e usando Componentes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import "./index.css";
+
+import Primeiro from "./components/basics/Primeiro.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+   <div>
+      <Primeiro></Primeiro>
+   </div>, 
+   document.querySelector('#root')
+)
